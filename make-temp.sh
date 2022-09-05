61061e4236f4c81b881113d47750b9138d53a0d3
c4arl0s <c.santiago.cruz@gmail.com>
2022-09-04 21:32:00 -0500

    Add how to use script

--- make-temp.sh
+++ make-temp.sh
@@ -1,34 +1,40 @@
-function makeTemp() {
-    [ -n $TMP ] || TMP='/tmp'
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Try to create a secure temp file name or directory
+# Called like: $temp_file=$(MakeTemp <file|dir> [path/to/name-prefix])
+# Returns the name of an a ra it in TEMP_NAME
+# For example:
+#        $temp_dir=$(MakeTemp dir /tmp/$PROGRAM.foo)
+#        $temp_file=$(MakeTemp file /tmp/$PROGRAM.foo)
+#
 
-    local type_name=$1
-    local prefix=${2:-$TMP/temp}
-    local temp_type=''
-    local sanity_check=''
+[ -n $TMP ] || TMP='/tmp'
+
+local type_name=$1
+local prefix=${2:-$TMP/temp}
+local temp_type=''
+local sanity_check=''
+
+case $type_name in
+    file )
+        temp_type=''
+        ur_cmd='touch'
+        sanity_check='test -f $TEMP_NAME -a -r $TEMP_NAME -a -w $TEMP_NAME -a -O $TEMP_NAME';;
+    dir|directory )
+        temp_type='-d'
+        ur_cmd='mkdir -p -m0700'
+        sanity_check='test -d $TEMP_NAME -a -r $TEMP_NAME -a -w $TEMP_NAME -a -x $TEMP_NAME -a -O $TEMP_NAME';;
+   *) Error "\nBad type in $PROGRAM:make-tem! Needs file|dir." 1;;
+esac
     
-    case $type_name in
-        file )
-            temp_type=''
-            ur_cmd='touch'
-            sanity_check='test -f $TEMP_NAME -a -r $TEMP_NAME -a -w $TEMP_NAME -a -O $TEMP_NAME';;
-        dir|directory )
-            temp_type='-d'
-            ur_cmd='mkdir -p -m0700'
-            sanity_check='test -d $TEMP_NAME -a -r $TEMP_NAME -a -w $TEMP_NAME -a -x $TEMP_NAME -a -O $TEMP_NAME';;
-       *) Error "\nBad type in $PROGRAM:make-tem! Needs file|dir." 1;;
-    esac
-        
-    TEMP_NAME=$(mktemp $temp_type ${prefix}.XXXXXXXXX)
-    if [ -z "$TEMP_NAME" ]
-    then
-         TEMP_NAME="${prefix}.$(cat /dev/urandom | od -x | tr -d ' ' | head -1)"
-         $ur_cmd $TEMP_NAME
-    fi     
-    if ! eval $sanity_check
-    then
-        Error "\aFATAL ERROR: can't create temp $type_name with '$0:MakeTemp$*'!\n" 2
-    else
-        echo "$TEMP_NAME"
-    fi
-}
-
+TEMP_NAME=$(mktemp $temp_type ${prefix}.XXXXXXXXX)
+if [ -z "$TEMP_NAME" ]
+then
+     TEMP_NAME="${prefix}.$(cat /dev/urandom | od -x | tr -d ' ' | head -1)"
+     $ur_cmd $TEMP_NAME
+fi     
+if ! eval $sanity_check
+then
+    Error "\aFATAL ERROR: can't create temp $type_name with '$0:MakeTemp$*'!\n" 2
+else
+    echo "$TEMP_NAME"
+fi
